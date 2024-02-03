12ce3c5c30b71c9635a3a02c8a44d6f8431c7e98
yusufkt0 <yusufkayrataner3@gmail.com>
2024-02-03 17:20:11 +0300

    optimization

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import cv2
 import cameradef as cam
 import mapdef as map
 
-vid = cv2.VideoCapture()
 frame = cv2.imread
 
 cam.getcam(0)
 
 cv2.waitKey(0)
-vid.release()
+cam.vid.release()
 cv2.destroyAllWindows()
